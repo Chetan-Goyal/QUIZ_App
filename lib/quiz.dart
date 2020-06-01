94202915d15d156e0ed37f9e96cff66bf2987d42
Chetan-Goyal <harshrock616@gmail.com>
2020-06-01 22:58:20 +0530

    Added Separate file for Question + Options Widget

---
+++ lib/quiz.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'question.dart';
+import 'answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List questions;
+  final Function answerQuestion;
+  final int questionIndex;
+  Quiz(
+      {@required this.questions,
+      @required this.answerQuestion,
+      @required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Question Here
+        Question(
+          this.questions[this.questionIndex]["QuestionText"],
+        ),
+
+        // Options Here
+        ...(this.questions[this.questionIndex]['Options'] as List<Map>)
+            .map((option) {
+          return Answer(answerQuestion, option["OptionText"], option["Score"]);
+        }).toList()
+      ],
+    );
+  }
+}
