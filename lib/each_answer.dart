f57e7d8e3ec736243175f0914497efc8a53e64ee
Chetan-Goyal <harshrock616@gmail.com>
2020-06-15 22:50:52 +0530

    Renamed File Added (answer.dart)

---
+++ lib/each_answer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
+
+class Answer extends StatelessWidget {
+  final String answer;
+  final Function selectHandler;
+
+  Answer(this.selectHandler, this.answer);
+
+  String _parseHtmlString(String htmlString) {
+    var text = html.Element.span()..appendHtml(htmlString);
+    return text.innerText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var container = Container(
+      child: RaisedButton(
+        child: Text(_parseHtmlString(answer), textAlign: TextAlign.center,),
+        onPressed: () => selectHandler(answer),
+        padding: EdgeInsets.all(5),
+      ),
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+      width: 200,
+    );
+    return container;
+  }
+}
