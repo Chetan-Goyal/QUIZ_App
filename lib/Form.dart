7e6419f7f5985495419d4d2e8d88f933592afd78
Chetan-Goyal <harshrock616@gmail.com>
2020-06-17 20:32:19 +0530

    Cateogry DropDown Menu Added

--- lib/Form.dart
+++ lib/Form.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'categories_data.dart';
 
-class UserInfo extends StatelessWidget {
+class UserInfo extends StatefulWidget {
   final Function _submitButton;
+  final Function categoryChanged;
   UserInfo(
     this._submitButton,
+    this.categoryChanged,
   );
+
+  @override
+  _UserInfoState createState() {
+    return _UserInfoState(this.categoryChanged);
+  }
+}
+
+class _UserInfoState extends State<UserInfo> {
+  final Function categoryChanged;
+  _UserInfoState(this.categoryChanged);
+
+  String _dropDownValue;
+
+  CategoriesData _dataObj = CategoriesData();
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +37,6 @@ class UserInfo extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-
         TextField(
           decoration: InputDecoration(hintText: 'Name'),
           onChanged: (String text) {
@@ -31,10 +47,47 @@ class UserInfo extends StatelessWidget {
           },
         ),
         SizedBox(height: 30),
+        Row(
+          children: <Widget>[
+            Text(
+              "Category:     ",
+              style: TextStyle(fontSize: 16),
+            ),
+            DropdownButton(
+              hint: _dropDownValue == null
+                  ? Text('Random Category')
+                  : Text(
+                      _dropDownValue,
+                      style: TextStyle(color: Colors.purple),
+                    ),
+              // isExpanded: true,
+              iconSize: 50.0,
+              itemHeight: 50,
+              dropdownColor: Colors.grey[350],
+              style: TextStyle(color: Colors.purple, fontSize: 16),
+              items: _dataObj.myList.keys.map(
+                (val) {
+                  return DropdownMenuItem<String>(
+                    value: val,
+                    child: Text(val),
+                  );
+                },
+              ).toList(),
+              onChanged: (val) {
+                setState(() {
+                  _dropDownValue = val;
+                  categoryChanged(_dataObj.myList[val]);
+                });
+              },
+              // },
+            )
+          ],
+        ),
+        SizedBox(height: 30),
         RaisedButton(
-            onPressed: () => _submitButton(value), child: Text("Submit")),
+            onPressed: () => widget._submitButton(value),
+            child: Text("Submit")),
       ],
-      
     ));
   }
 }
