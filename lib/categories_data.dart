74aa9393621811473c0fea947b399476b04d8e3a
Chetan-Goyal <harshrock616@gmail.com>
2020-06-17 20:27:02 +0530

    Categories' Data Loaded

---
+++ lib/categories_data.dart
@@ -0,0 +1,30 @@
+class CategoriesData {
+  
+  var myList = {
+    "Mixed QUIZ" : "any",
+    "General Knowledge" : "9",
+    "Books" : "10",
+    "Movies" : "11",
+    "Music" : "12",
+    "Television" : "14",
+    "Video Games" : "15",
+    "Board Games" : "16",
+    "Science and Nature" : "17",
+    "Computers" : "18",
+    "Mathematics" : "19",
+    "Mythology" : "20",
+    "Sports" : "21",
+    "Geography" : "22",
+    "History" : "23",
+    "Politics" : "24",
+    "Art" : "25",
+    "Celebrities" : "26",
+    "Animals" : "27",
+    "Vehicles" : "28",
+    "Comics" : "29",
+    "Gadgets" : "30",
+    "Japanese Anime and Manga" : "31",
+    "Cartoon and Animations" : "31",
+  };
+}
+
