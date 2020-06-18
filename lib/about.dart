0c4093f47425d14ae2d2d1300d222ba135dcee09
Chetan-Goyal <harshrock616@gmail.com>
2020-06-19 01:40:35 +0530

    Sample About Page Added

---
+++ lib/about.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About"),
+      ),
+      body: Container(
+        child: Text("About Page"),
+      ),
+    );
+  }
+}
